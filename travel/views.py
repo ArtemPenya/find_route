79b355f345535f99125ffe80067ce22bacd7ab9b
ArtemPenya <artempenya@gmail.com>
2022-02-16 11:28:41 +0300

    third commit

--- travel/views.py
+++ travel/views.py
@@ -6,3 +6,9 @@ def home(request):
     name = 'chlen'
 
     return render(request, 'home.html', {'name': name})
+
+
+def about(request):
+    name = 'about us'
+
+    return render(request, 'about.html', {'name': name})
