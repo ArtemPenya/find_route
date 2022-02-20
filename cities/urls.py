d7d008a7cc716f50f0e3751ace0f8a8b6ace35ec
ArtemPenya <artempenya@gmail.com>
2022-02-20 12:29:23 +0300

    third commit

---
+++ cities/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from cities.views import *
+
+urlpatterns = [
+    # /path('', trains, name='trains'),
+    path('', CityListView.as_view(), name='cities'),
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
+    path('add/', CityCreateView.as_view(), name='create'),
+
+]
--- travel/urls.py
+++ travel/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from views import home, about
+from travel.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('cities/', include('cities.urls')),
+    path('', home, name='home'),
     path('about/', about)
 ]
